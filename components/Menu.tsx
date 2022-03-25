60fffefc75546d44cd8421b89404ba5d29f71c2e
Yann Haefliger <yhaefliger@gmail.com>
2022-03-25 16:56:41 +0100

    menus progress

---
+++ components/Menu.tsx
@@ -0,0 +1,39 @@
+import { useAppContext } from "contexts/AppContext"
+import { useRouter } from "next/router"
+
+
+const MenuLinks = ({ links, level = 0 }) => {
+  return (
+    <>
+      {links.map((item, index) => {
+          return (
+            <li key={index}>
+              {!!item.links && !!item.links.length && (
+                <ul>
+                  <MenuLinks links={item.links} level={level + 1} />
+                </ul>
+              )}
+            </li>
+          )
+      })}
+    </>
+  )
+}
+
+const Menu = ({ name }) => {
+  const { menus } = useAppContext()
+  const router = useRouter()
+  const menu = menus[name]
+
+  if (!menu || !menu.links || !menu.links?.length) {
+    return null
+  }
+
+  return (
+    <div className="flex">
+      <MenuLinks links={menu.links} level={0} />
+    </div>
+  )
+}
+
+export default Menu
